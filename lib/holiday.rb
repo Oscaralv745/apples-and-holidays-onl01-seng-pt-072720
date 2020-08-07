7f05c8b3abc07b5dffaec8d588a30410b8b6759c
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-07 02:13:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday.map do 
+  holiday_hash.map do |holiday, season|
+    holiday.map do 
 end
 
 
