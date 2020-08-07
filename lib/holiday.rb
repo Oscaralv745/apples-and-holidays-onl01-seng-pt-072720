5b806f7c8cd5a16c676a83f87cb660985c5a6a4b
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-07 02:12:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,10 +59,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |holiday, item|
-      puts " #{holiday.to_s.split("_").map {|i|i.capitalize}.join(" ")}: #{item.join(", ")}"
-    end
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
   end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
